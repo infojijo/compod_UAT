d63bc06ec93591ce9dfec11fc01e37d871ab73df
Jijo Jose <info.jijo@gmail.com>
2020-08-26 09:22:15 -0500

    datepicker

--- src/test/java/com/cjnet/iOS_NativeAPP_Test1.java
+++ src/test/java/com/cjnet/iOS_NativeAPP_Test1.java
@@ -2,6 +2,7 @@ package com.cjnet;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -43,16 +44,37 @@ public WebDriverWait wait;
 		  wait = new WebDriverWait(iOSDriver,20);	  
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Attributes")));
 		  //iOSDriver.findElement(By.name("Attributes")).sendKeys("admin");
-		  iOSDriver.findElement(By.name("Attributes")).click();
-		  
+		  iOSDriver.findElement(By.name("Attributes")).click();		  
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("aIdentifier")));
 		  
 		  iOSDriver.findElement(By.name("aIdentifier")).click();
 		  iOSDriver.findElement(By.name("aIdentifier")).sendKeys("iOS Automation");
 		  iOSDriver.findElement(By.name("Button")).click();
+		  iOSDriver.findElement(By.xpath("//XCUIElementTypeButton[@name=\"Second\"]")).click();	  
+		  iOSDriver.findElementByXPath("//XCUIElementTypeSwitch").click();		  
+		  //iOSDriver.findElementByXPath("//XCUIElementTypeSlider").click();	
+		  iOSDriver.findElementByXPath("//XCUIElementTypeSlider").sendKeys("0.7");
 		  
-
-		  		  
+		  
+		  String progress = iOSDriver.findElement(By.xpath("//XCUIElementTypeProgressIndicator[@name=\"Progress\"]")).getText();
+		  System.out.println("Progress Value->"+progress);
+		  
+		  List<MobileElement> pickerWheel = iOSDriver.findElementsByXPath("//XCUIElementTypePickerWheel");
+		  pickerWheel.get(0).click();  
+		  
+		  //use the selectPickerWheelValue method to move to the next value in the 'month' wheel
+	      HashMap<String, Object> params = new HashMap<>();
+	      params.put("order", "next");
+	      params.put("offset", 0.15);
+	      params.put("element", ((MobileElement) pickerWheel.get(0)).getId());
+	      iOSDriver.executeScript("mobile: selectPickerWheelValue", params);
+		  
+		  
+		  pickerWheel.get(1).sendKeys("1");
+		  pickerWheel.get(2).sendKeys("11");
+		  pickerWheel.get(3).sendKeys("PM");
+		  //MobileElement progressBar = (MobileElement) iOSDriver.findElement(By.xpath("//XCUIElementTypeProgressIndicator[@name=\"Progress\"]"));
+		  //progressBar.sendKeys("90%");
 		  
 	}
 	
